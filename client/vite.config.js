68fd2ddaf93aa1097a64fe653c0ba4de5eedf83a
Saif <Saifsep17@gmail.com>
2024-03-30 10:54:58 +0530

    First Commit, Initializing the project

---
+++ client/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  proxy: {
+    "/server": {
+      target: "http://localhost/3000",
+      secure: false,
+    },
+  },
+  plugins: [react()],
+});
