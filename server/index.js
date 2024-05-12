247213c2cc7caa06d88ca50c9f12131c17ea4323
Saif <Saifsep17@gmail.com>
2024-05-12 11:42:14 +0530

    Changed path of client

--- server/index.js
+++ server/index.js
@@ -47,8 +47,8 @@ app.use("/server", signoutRouter);
 app.use("/server", getFoodRouter);
 app.use("/server/order", orderRouter);
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
 });
