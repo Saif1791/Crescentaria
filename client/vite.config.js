9ec16c510a7439ce670b93ea4e30b9e749470f28
Saif <Saifsep17@gmail.com>
2024-05-22 16:42:12 +0530

    Added support for modern ES features

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
+  build: {
+    target: "esnext", //browsers can handle the latest ES features
+  },
   server: {
     proxy: {
       "/server": {
